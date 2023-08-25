0e7d5fe6c7bd6c52699a19e55609def148c453ea
Vlad <vld07052008@yandex.ru>
2023-08-25 13:01:12 +0300

    added new app

--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'advertisements81',
+    'app_advertisements',
     'app_lesson_4',
 ]
 
@@ -121,6 +121,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     Path.joinpath(BASE_DIR, 'static'),
 ]
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ advertisements/app_advertisements/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+from .models import Advertisements81
+from .forms import Advertisements81Form
+
+def index(request):
+    advertisements = Advertisements81.objects.all()
+    context = {'advertisements':advertisements}
+    return render(request, 'index.html', context)
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+
+def advertisement_post(request):
+    form = Advertisements81Form()
+    context = {'form' : form}
+    return render(request, 'advertisement-post.html', context)
+
+def advertisements_post(request):
+    if request.method == "POST":
+        form = Advertisements81Form(request.POST, request.FILES)
+        if form.is_valid():
+            advertisement = Advertisements81(**form.cleaned_data)
+            advertisement.user = request.user
+            advertisement.save()
+            url = reverse('main-page')
+            return HttpResponseRedirect(url)
+    else:
+        form = Advertisements81Form()
+    context = {'form': form}
+    return render(request, 'advertisement_post.html')
